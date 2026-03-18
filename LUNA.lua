ce054b70dd140d8a2a3b69a644d3f097af8a3299
Crwth-sl <crwth.sl@gmail.com>
2026-03-18 06:39:48 +0000

    Update LUNA.lua

--- LUNA.lua
+++ LUNA.lua
@@ -1563,16 +1563,18 @@ local IconModule = {
 local request = (syn and syn.request) or (http and http.request) or http_request or nil
 local tweeninfo = TweenInfo.new(0.3, Enum.EasingStyle.Exponential, Enum.EasingDirection.Out)
 local PresetGradients = {
-	["Nightlight (Classic)"] = {Color3.fromRGB(147, 255, 239), Color3.fromRGB(201,211,233), Color3.fromRGB(255, 167, 227)},
-	["Nightlight (Neo)"] = {Color3.fromRGB(117, 164, 206), Color3.fromRGB(123, 201, 201), Color3.fromRGB(224, 138, 175)},
-	Starlight = {Color3.fromRGB(147, 255, 239), Color3.fromRGB(181, 206, 241), Color3.fromRGB(214, 158, 243)},
-	Solar = {Color3.fromRGB(242, 157, 76), Color3.fromRGB(240, 179, 81), Color3.fromRGB(238, 201, 86)},
-	Sparkle = {Color3.fromRGB(199, 130, 242), Color3.fromRGB(221, 130, 238), Color3.fromRGB(243, 129, 233)},
-	Lime = {Color3.fromRGB(170, 255, 127), Color3.fromRGB(163, 220, 138), Color3.fromRGB(155, 185, 149)},
-	Vine = {Color3.fromRGB(0, 191, 143), Color3.fromRGB(0, 126, 94), Color3.fromRGB(0, 61, 46)},
-	Cherry = {Color3.fromRGB(148, 54, 54), Color3.fromRGB(168, 67, 70), Color3.fromRGB(188, 80, 86)},
-	Daylight = {Color3.fromRGB(51, 156, 255), Color3.fromRGB(89, 171, 237), Color3.fromRGB(127, 186, 218)},
-	Blossom = {Color3.fromRGB(255, 165, 243), Color3.fromRGB(213, 129, 231), Color3.fromRGB(170, 92, 218)},
+    ["Nightlight Neo"] = {Color3.fromRGB(117,164,206), Color3.fromRGB(123,201,201), Color3.fromRGB(224,138,175)},
+    Solar = {Color3.fromRGB(242,157,76), Color3.fromRGB(240,179,81), Color3.fromRGB(238,201,86)},
+    Blossom = {Color3.fromRGB(255,165,243), Color3.fromRGB(213,129,231), Color3.fromRGB(170,92,218)},
+    Vine = {Color3.fromRGB(0,191,143), Color3.fromRGB(0,126,94), Color3.fromRGB(0,61,46)},
+    Cherry = {Color3.fromRGB(148,54,54), Color3.fromRGB(168,67,70), Color3.fromRGB(188,80,86)},
+    Daylight = {Color3.fromRGB(51,156,255), Color3.fromRGB(89,171,237), Color3.fromRGB(127,186,218)},
+    Blood = {Color3.fromRGB(220,0,0), Color3.fromRGB(150,0,0), Color3.fromRGB(80,0,0) },
+    Grape = {Color3.fromRGB(180,90,255), Color3.fromRGB(140,60,210), Color3.fromRGB(100,40,170)},
+    Ocean = {Color3.fromRGB(0,200,255), Color3.fromRGB(0,140,220), Color3.fromRGB(0,90,180)},
+    Sentinel = {Color3.fromRGB(255,60,90), Color3.fromRGB(200,40,70), Color3.fromRGB(140,20,50)},
+    Synapse = {Color3.fromRGB(139,94,60), Color3.fromRGB(110,70,45), Color3.fromRGB(80,50,30)},
+    Serpent = {Color3.fromRGB(0,220,120), Color3.fromRGB(0,170,90), Color3.fromRGB(0,120,60)},
 }
 
 local function GetIcon(icon, source)
