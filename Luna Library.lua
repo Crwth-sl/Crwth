23efb1fde4308e5446b87b1bf260fd7aab0832fe
Crwth-sl <crwth.sl@gmail.com>
2026-03-18 06:06:20 +0000

    Update Luna Library.lua

--- Luna Library.lua
+++ Luna Library.lua
@@ -6557,7 +6557,7 @@ function Luna:CreateWindow(WindowSettings)
             for _, obj in pairs(LunaUI:GetDescendants()) do
 
                 -- ❌ skip main frame
-                if obj.Name == "MainFrame" then
+                if obj.Name == "Main" then
                     continue
                 end
 
