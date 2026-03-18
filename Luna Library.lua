03e2404b0cca08f3555a6f634ca4c461a8e938e0
Crwth-sl <crwth.sl@gmail.com>
2026-03-18 06:12:53 +0000

    Update Luna Library.lua

--- Luna Library.lua
+++ Luna Library.lua
@@ -6509,18 +6509,6 @@ function Luna:CreateWindow(WindowSettings)
                 if obj:IsA("UIGradient") then
                     obj.Color = self.ThemeGradient
                 end
-
-                if obj:IsA("Frame") then
-                    obj.BackgroundColor3 = self.ThemeGradient
-                end
-
-                if obj:IsA("TextButton") or obj:IsA("ImageButton") then
-                    obj.BackgroundColor3 = self.ThemeGradient
-                end
-
-                if obj:IsA("TextLabel") then
-                    obj.TextColor3 = self.ThemeGradient
-                end
             end
         end
 
