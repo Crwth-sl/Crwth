b672a5bea67f6ae66f1aacae6575b8c90fddeb53
Crwth-sl <crwth.sl@gmail.com>
2026-03-18 06:16:43 +0000

    Update Luna Library.lua

--- Luna Library.lua
+++ Luna Library.lua
@@ -6520,9 +6520,6 @@ function Luna:CreateWindow(WindowSettings)
 
     for _, obj in pairs(descendants) do
 
-        if obj:IsA("Frame") then
-            obj.BackgroundColor3 = softenColor(self.ThemeGradient.Keypoints[1].Value)
-        end
 
         if obj:IsA("TextButton") or obj:IsA("ImageButton") then
             obj.BackgroundColor3 = softenColor(self.ThemeGradient.Keypoints[2].Value)
